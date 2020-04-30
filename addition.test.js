f1b6b7fde853e95401743b7e42759d2f3b3719ba
Jasonlin1198 <jasonlin1198@gmail.com>
2020-04-30 15:47:08 -0700

    linted

--- addition.test.js
+++ addition.test.js
@@ -1,5 +1,5 @@
-const addition = require("./addition");
+const addition = require('./addition');
 
-test("adds 1 + 2 to equal 3", () => {
+test('adds 1 + 2 to equal 3', () => {
   expect(addition(1, 2)).toBe(3);
 });
