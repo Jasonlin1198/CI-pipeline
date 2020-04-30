1b542dee3c76eb5f372d1d4b27f9b1b3ad763078
Jasonlin1198 <jasonlin1198@gmail.com>
2020-04-30 15:25:15 -0700

    add linter

--- add.js
+++ add.js
@@ -1,6 +1,11 @@
+/** This function adds numbers
+ * @param {int} a - first num to add
+ * @param {int} b - second num to add
+ * @return {int} - sum of a and b
+ */
 function addition(a, b) {
   return a + b;
 }
 
 console.log(addition(2, 3));
-console.log("Hello world");
+console.log('Hello world');
